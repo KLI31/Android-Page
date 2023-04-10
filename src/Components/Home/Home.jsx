8e647fc4cf72a3c2c9df6ca17fcba983f4c7dc04
KLI-31 <lrambao5432@gmail.com>
2023-04-09 21:51:04 -0500

    :recycle: improve Home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import "./Home.css"
+import image from "../../Assets/Cellphone.png"
 
 const Home = () => {
   return (
-    <div className=''>Home</div>
+    <div className='home'>
+      <div className='secContainer'>
+        <div className="homeText">
+          <span className="homeSpan">
+            Meet your ideal phone
+          </span>
+          <h1 className='homeTitle'>
+            Xiaomi Redmi 9S
+          </h1>
+          <div className="btns flex">
+            <button className="btn">More Details</button>
+            <button className="btn primaryBtn">Test Cellphone</button>
+          </div>
+        </div>
+
+        <div className="homeImage">
+          <img src={image} alt="homeImage" className='image' />
+        </div>
+      </div>
+    </div>
   )
 }
 
