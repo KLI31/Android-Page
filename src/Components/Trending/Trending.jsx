2627a02c8ddd65ae5cbc8939f0723fb2206d403a
KLI-31 <lrambao5432@gmail.com>
2023-04-09 21:56:51 -0500

    :construction: work in progress

--- src/Components/Trending/Trending.jsx
+++ src/Components/Trending/Trending.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import "./Trendings.css"
+import {BsArrowLeftShort, BsArrowRightShort} from "react-icons/bs"
 
 const Trending = () => {
   return (
-    <div className=''>Trending</div>
+    <div className='trending section'>
+      <div className="secContainer container">
+        <div className="secHeading flex">
+          <h3 className="secTitle">
+            Treding Near You
+          </h3>
+
+          <div className="navBtns flex">
+                <BsArrowLeftShort className="icon leftIcon"/>
+                <BsArrowRightShort className="icon rightIcon"/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
