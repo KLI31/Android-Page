a2ea9e065149f80eac9cdcc03745baf33361707a
KLI-31 <lrambao5432@gmail.com>
2023-04-09 21:54:04 -0500

    :recycle: improve component navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,40 +3,52 @@ import "./Navbar.css"
 import logo from "../../Assets/logo2.svg"
 import {RiCloseCircleFill} from "react-icons/ri"
 import {TbGridDots} from "react-icons/tb"
+import { useState } from 'react'
 
 
 const Navbar = () => {
+
+  const [navbar, setNavbar] = useState("navbar")
+
+  const showNavbar = () => {
+    setNavbar("navbar showNavbar")
+  }
+
+  const closeNabvar =() =>{
+    setNavbar("navbar")
+  }
+
   return (
     <div className='header'>
       <div className='containerLogo'>
           <img src={logo} alt="logo-image" className='logo' />
       </div>
 
-      <div className='navbar'>
+      <div className={navbar}>
         <ul className='menu'>
-          <li className='listItem'>
+          <li onClick={closeNabvar} className='listItem'>
           <a href='/' className='link'>Cellphones</a>
           </li>
 
-          <li className='listItem'>
+          <li onClick={closeNabvar} className='listItem'>
           <a href='/' className='link'>New Cellphones</a>
           </li>
 
-          <li className='listItem'>
+          <li onClick={closeNabvar} className='listItem'>
           <a href='/' className='link'>Auction</a>
           </li>
 
-          <li className='listItem'>
+          <li onClick={closeNabvar} className='listItem'>
           <a href='/' className='link'>Sell</a>
           </li>
         </ul>
 
-        <RiCloseCircleFill className="icon closeIcon"/>
+        <RiCloseCircleFill className="icon closeIcon" onClick={closeNabvar}/>
       </div>
 
        <div className="signUp flex">
           <div className="text">Sign up</div>
-          <TbGridDots className="icon toggleNavbarIcon"/>
+          <TbGridDots className="icon toggleNavbarIcon" onClick={showNavbar}/>
       </div> 
     </div>
   )
